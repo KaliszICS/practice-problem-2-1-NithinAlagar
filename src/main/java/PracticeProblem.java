b93e53dbd87df615739aa60f69610aacdf699e5a
NithinAlagar <343954178@tdsb.ca>
2026-03-10 15:27:36 +0000

    Nithin.A

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,9 @@
+/**
+ * File: Converting Data Types
+ * Author: Nithin.A
+ * Date Created: Mar 4, 2026
+ * Date Last Modified: Mar 10, 2026
+ */
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -5,5 +11,20 @@ public class PracticeProblem {
 	}
 
 	//Create your functions here
-
+	public static int sum (int num1, int num2){
+		int result = num1 + num2;
+		return result;
+	}
+	public static int difference (int num1, int num2){
+		int result = num1 - num2;
+		return result;
+	}
+	public static double product (double num1, double num2){
+		double result = num1*num2;
+		return result;
+	}
+	public static String removeFirst (String num1){
+		String result = num1.substring(1);
+		return result;
+	}
 }
